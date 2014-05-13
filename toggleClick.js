20e1da898a73b86a1e89f899ea1174ce2b42f1a5
JÎÌKE <jake.wolpert@me.com>
2014-05-13 13:58:05 -0700

    toggleClick
    Toggle is dead. Long live toggleClick!

---
+++ toggleClick.js
@@ -0,0 +1,11 @@
+;(function($){
+	$.fn.toggleClick=function(){
+		var functions=arguments
+		return this.click(function(){
+			var iteration=$(this).data('iteration')||0
+			functions[iteration].apply(this,arguments)
+			iteration= (iteration+1) %functions.length
+			$(this).data('iteration',iteration)
+		})
+	}
+})(jQuery)
